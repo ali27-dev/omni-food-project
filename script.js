0a5a250e2f027802edd6b12478ebb8215040d1a3
Abid ali <khanali3229>
2024-07-07 09:02:51 +0500

    i do some changes in html nav-section and give smooth scrolling with help of javaScript

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ const headerEl = document.querySelector(".header");
 btnnavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+// ************************* //
+// smooth-scrolling
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    //mobile nav
+
+    if (link.classList.contains("nav-li-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
